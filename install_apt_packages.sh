0e9bacd7479ec4e43453f35bc2f6df6a1c5d2884
Jelte Fennema <jelte12345@gmail.com>
2012-12-27 06:29:06 +0100

    Add some info messages, games and some more utilities

--- install_apt_packages.sh
+++ install_apt_packages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Sign Google certificate
+echo 'Setting up Google Chrome stuff'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo \
     apt-key add -
 
@@ -12,19 +13,27 @@ echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | tee \
 sudo apt-get update
 
 # Install everything
+echo 'Installing important stuff'
 sudo apt-get --yes --force-yes install vim ack-grep git keepass2 python-pip\
     ruby-sass irssi htop ipython synapse xdotool mono-complete gnuplot pypy\
-    python-dev encfs git-flow tig google-chrome-stable
+    python-dev encfs git-flow tig google-chrome-stable gparted skype wine\
+    audacity
+
+# Ask confirmation for games
+echo 'Do you want to install games?'
+sudo apt-get install openarena assaultcube
 
 # Add keepass plugins
 sudo wget https://github.com/pfn/keepasshttp/raw/master/KeePassHttp/KeePassHttp.dll -O /usr/lib/keepass2/KeePassHttp.dll
 sudo wget https://github.com/pfn/keepasshttp/raw/master/KeePassHttp/Newtonsoft.Json.dll -O /usr/lib/keepass2/Newtonsoft.Json.dll
 
 # Upgrade all existing packeges to newest version
+echo 'Updating all packages'
 sudo apt-get --yes --force-yes upgrade
 
 # Install extra python packages
 pip install --user sqlalchemy
 
 # Install all the LaTeX, but ask
+echo 'Do you really want ALL the LaTeX?'
 sudo apt-get install texlive-*
