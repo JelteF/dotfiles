78580cf918dc8912a3704ae5c3b049e542ccca8c
Jelte Fennema <jelte12345@gmail.com>
2013-05-16 01:22:15 +0200

    Reworked portage install script to make it more easy to modify and a lot less cluttered, also added more packages

--- install_portage_packages.sh
+++ install_portage_packages.sh
@@ -1,3 +1,27 @@
 #/bin/bash
 
-emerge vim wpa_supplicant awesome xorg-server git tig skype flask flask-sqlalchemy slim synapse ack the_silver_searcher keepass dev-python/pip sass irssi htop ipython sci-visualization/gnuplot pypy gparted wine audacity layman texlive firefox libreoffice gnome-power-manager evince -a --update --deep --newuse world
+# Base
+VIM="vim vim-spell-nl vim-spell-en"
+GENTOO_UTILS="layman gentoolkit"
+BASH_UTILS="ack the_silver_searcher irssi htop wireless-tools pciutils"
+GIT="git tig"
+
+# X
+X_BASE="awesome slim xorg-server volumeicon"
+X_UTILS="synapse nautilus keepass gparted evince google-chrome"
+X_FONTS="ubuntu-font-family"
+X_TERMS="xterm gnome-terminal"
+
+# Extra
+PYTHON_UTILS="pypy dev-python/pip ipython"
+WEBDEV="flask flask-sqlalchemy sass"
+EXTRA="skype sci-visualization/gnuplot wine audacity texlive"
+LARGE="firefox libreoffice"
+
+LAPTOP="wpa_supplicant gnome-power-manager"
+
+
+
+# Install all
+emerge $VIM $GENTOO_UTILS $BASH_UTILS $GIT $X_BASE $X_UTILS $X_FONTS $X_TERMS \
+$PYTHON_UTILS $WEBDEV $EXTRA $LARGE $LAPTOP -a --update --deep --newuse world
