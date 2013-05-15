309efe0bd16ee539636d2901d1c1c5c276c222be
Jelte Fennema <jelte12345@gmail.com>
2013-05-15 10:56:44 +0200

    Add portage files

---
+++ install_portage_packages.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+emerge vim wpa_supplicant awesome xorg-server git tig skype flask flask-sqlalchemy slim synapse ack the_silver_searcher keepass dev-python/pip sass irssi htop ipython sci-visualization/gnuplot pypy gparted wine audacity layman texlive firefox libreoffice gnome-power-manager evince -a --update --deep --newuse world
