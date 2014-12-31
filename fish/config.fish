62977c2a56f27d2dd71afd50c6661547f01a3536
Via Git <git@viaserver>
2014-12-31 15:39:05 +0100

    Make su use my own special root user

--- fish/config.fish
+++ fish/config.fish
@@ -21,6 +21,14 @@ set -x ECHANGELOG_USER "Jelte Fennema (JelteF) <github-tech@jeltef.nl>"
 # Disable clipboard polution
 set FISH_CLIPBOARD_CMD "cat"
 
+function su
+    if test (count $argv) = 0
+        command su jelte_root
+    else
+        command su $argv
+    end
+end
+
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
