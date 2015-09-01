78787900c2aedb296e252989e415ac430e60fab3
Jelte Fennema <github-tech@jeltef.nl>
2015-09-01 15:27:08 +0200

    Update ubuntu installer file

--- install_apt_packages.sh
+++ install_apt_packages.sh
@@ -9,6 +9,17 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo \
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee \
     /etc/apt/sources.list.d/google.list > /dev/null
 
+# Add insync stuff
+echo 'Setting up insync stuff'
+wget -qO - https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key \
+    | sudo apt-key add -
+
+echo "deb http://apt.insynchq.com/ubuntu vivid non-free contrib" | sudo tee \
+    /etc/apt/sources.list.d/insync.list > /dev/null
+
+echo 'Setting up fish'
+sudo apt-add-repository ppa:fish-shell/release-2
+
 # Update sources
 sudo apt-get update
 
@@ -16,23 +27,19 @@ sudo apt-get update
 echo 'Installing important stuff'
 sudo apt-get --yes --force-yes install vim ack-grep git keepass2 python-pip\
     ruby-sass irssi htop ipython synapse xdotool mono-complete gnuplot pypy\
-    python-dev encfs git-flow tig google-chrome-stable gparted skype wine\
-    audacity
+    python-dev git-flow tig google-chrome-stable gparted insync curl fish
 
 # Ask confirmation for games
 echo 'Do you want to install games?'
 sudo apt-get install openarena assaultcube
 
 # Add keepass plugins
-sudo wget https://github.com/pfn/keepasshttp/raw/master/KeePassHttp/KeePassHttp.dll -O /usr/lib/keepass2/KeePassHttp.dll
-sudo wget https://github.com/pfn/keepasshttp/raw/master/KeePassHttp/Newtonsoft.Json.dll -O /usr/lib/keepass2/Newtonsoft.Json.dll
+sudo wget https://github.com/pfn/keepasshttp/raw/master/KeePassHttp.plgx -O /usr/lib/keepass2/KeePassHttp.plgx
+sudo chmod 644 /usr/lib/keepass2/KeePassHttp.plgx
 
 # Upgrade all existing packeges to newest version
 echo 'Updating all packages'
 sudo apt-get --yes --force-yes upgrade
-
-# Install extra python packages
-pip install --user sqlalchemy
 
 # Install all the LaTeX, but ask
 echo 'Do you really want ALL the LaTeX?'
