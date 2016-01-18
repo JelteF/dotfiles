6b7133044cedb9462c46d32491bf018d470b5452
Jelte Fennema <github-tech@jeltef.nl>
2016-01-18 18:34:35 +0100

    Install dotfiles for neovim

--- setup.sh
+++ setup.sh
@@ -16,19 +16,22 @@ ORIGINAL_XPROFILE=$HOME/.xprofile
 ORIGINAL_XSESSION=$HOME/.xsession
 
 # Create vim folders
-mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/{autoload,colors}
+ln -s ~/.vim ~/.config/nvim
 mkdir -p ~/.ptpython
-mkdir -p ~/.vim/colors
 mkdir -p ~/.config/fish/completions
 
-# Install vundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+# Install vim-plug
+wget -O ~/.config/nvim/autoload/plug.vim \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install git-flow completion
-wget https://github.com/bobthecow/git-flow-completion/raw/master/git.fish -O ~/.config/fish/completions/git.fish
+wget https://github.com/bobthecow/git-flow-completion/raw/master/git.fish \
+    -O ~/.config/fish/completions/git.fish
 
 # Install nice color scheme
-wget https://raw.github.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night-Bright.vim -O ~/.vim/colors/Tomorrow-Night-Bright.vim
+wget https://raw.github.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night-Bright.vim \
+    -O ~/.vim/colors/Tomorrow-Night-Bright.vim
 
 ln -sf $PWD/bash/bashrc $ORIGINAL_BASHRC
 ln -sf $PWD/bash/bash_profile $ORIGINAL_BASH_PROFILE
@@ -37,6 +40,8 @@ ln -sf $PWD/bash/inputrc $ORIGINAL_INPUTRC
 ln -sf $PWD/git/gitconfig $ORIGINAL_GITCONFIG
 ln -sf $PWD/tmux/tmux.conf $ORIGINAL_TMUXCONF
 ln -sf $PWD/vim/vimrc $ORIGINAL_VIMRC
+ln -sf $PWD/vim/vimrc $HOME/.vim/init.vim
+ln -sf $PWD/fish/config.fish $ORIGINAL_FISHCONF
 ln -sf $PWD/x/XCompose $ORIGINAL_XCOMPOSE
 ln -sf $PWD/x/Xresources $ORIGINAL_XRESOURCES
 ln -sf $PWD/x/xinitrc $ORIGINAL_XINITRC
@@ -47,11 +52,13 @@ ln -sf $PWD/ptpython/* ~/.ptpython/
 ln -sf $PWD/config_dir/* ~/.config/
 
 # Install fisherman
-# git clone https://github.com/fisherman/fisherman ~/.fisherman
-cd ~/.fisherman
-make
-cd -
+if [ ! -d ~/.fisherman ]; then
+    git clone https://github.com/fisherman/fisherman ~/.fisherman
+    cd ~/.fisherman
+    make
+    cd -
 
-ln -sf $PWD/fish/config.fish $ORIGINAL_FISHCONF
+    ln -sf $PWD/fish/config.fish $ORIGINAL_FISHCONF
 
-exec fish < /dev/tty
+    exec fish < /dev/tty
+fi
