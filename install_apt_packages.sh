3272a9fafc2b506a34bd1c239806c9890b327a7f
Jelte Fennema <github-tech@jeltef.nl>
2016-02-12 15:50:10 +0100

    Ubuntu: Add extra tools to install

--- install_apt_packages.sh
+++ install_apt_packages.sh
@@ -19,4 +19,4 @@ sudo apt-get --yes --force-yes upgrade
 
 # Install all the LaTeX, but ask
 echo 'Do you really want ALL the LaTeX?'
-sudo apt-get install texlive-full
+sudo apt-get install texlive-full biber
