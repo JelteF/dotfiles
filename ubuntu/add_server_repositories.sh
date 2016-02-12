f534475da701b25844d045177a14537d4bb6224a
Jelte Fennema <github-tech@jeltef.nl>
2016-02-12 16:36:50 +0100

    Ubuntu: Say yes to fish ppa automatically

--- ubuntu/add_server_repositories.sh
+++ ubuntu/add_server_repositories.sh
@@ -4,8 +4,7 @@
 sudo apt-get install software-properties-common python-software-properties
 
 echo 'Setting up fish'
-sudo apt-add-repository ppa:fish-shell/release-2
+sudo apt-add-repository -y ppa:fish-shell/release-2
 
-sudo apt-get install -y python-software-properties software-properties-common
 echo 'Tmux repo'
 sudo add-apt-repository -y ppa:pi-rho/dev
