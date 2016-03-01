ebfe3b2a734f4d996667e8f84eec5320eb369f1d
Jelte Fennema <github-tech@jeltef.nl>
2016-03-01 12:40:08 +0100

    Automatic yes and add neovim

--- ubuntu/add_server_repositories.sh
+++ ubuntu/add_server_repositories.sh
@@ -3,7 +3,10 @@
 # Add appt-add-repository
 sudo apt-get install software-properties-common python-software-properties
 
-echo 'Setting up fish'
+echo 'Neovim repo'
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+
+echo 'Fish repo'
 sudo apt-add-repository -y ppa:fish-shell/release-2
 
 echo 'Tmux repo'
