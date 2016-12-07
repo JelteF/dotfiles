0360c47c0a9e22f983f26194ee136ea74e61e87a
Jelte Fennema <github-tech@jeltef.nl>
2016-12-07 10:35:58 +0100

    Ptpython: change color scheme

--- ptpython/config.py
+++ ptpython/config.py
@@ -4,7 +4,7 @@ __all__ = (
 
 
 def configure(repl):
-    repl.use_code_colorscheme('friendly')
+    repl.use_code_colorscheme('paraiso-dark')
     repl.enable_auto_suggest = True
     repl.enable_history_search = True
     repl.vi_mode = True
