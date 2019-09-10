345f5c0f2d7a4ab6a4c12b5fa378b3ec858ac0a7
Jelte Fennema <github-tech@jeltef.nl>
2019-09-10 15:03:32 +0200

    Update ubuntu install script

--- ubuntu/add_server_repositories.sh
+++ ubuntu/add_server_repositories.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+set -eux
+
 # Add appt-add-repository
-sudo apt-get install software-properties-common python-software-properties
+sudo apt-get install software-properties-common
 
 echo 'Neovim repo'
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 
 echo 'Fish repo'
-sudo apt-add-repository -y ppa:fish-shell/release-2
-
-echo 'Tmux repo'
-sudo add-apt-repository -y ppa:pi-rho/dev
+sudo apt-add-repository -y ppa:fish-shell/release-3
