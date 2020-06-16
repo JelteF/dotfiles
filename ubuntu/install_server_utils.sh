63160df5be99fe6e92eb6ab7055b76642519c226
Jelte Fennema <github-tech@jeltef.nl>
2020-06-16 18:32:16 +0200

    Add libssl-dev and pkg-config

--- ubuntu/install_server_utils.sh
+++ ubuntu/install_server_utils.sh
@@ -4,7 +4,7 @@ echo 'Installing important stuff'
 sudo apt-get --yes --force-yes install vim git python-pip python3-pip\
     htop python-dev python3-dev tig curl fish silversearcher-ag build-essential\
     tmux whois wget neovim cmake clang-tools-8 zlib1g-dev libncurses-dev gdb\
-    shellcheck openssl
+    shellcheck openssl libssl-dev pkg-config
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.0.1/ripgrep_12.0.1_amd64.deb
 
