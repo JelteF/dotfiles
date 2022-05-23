f91819bc116acb2cd47df2168619cdce2bfe36f4
Jelte Fennema <github-tech@jeltef.nl>
2022-05-23 10:03:33 +0200

    Use stable version of neovim

--- ubuntu/add_server_repositories.sh
+++ ubuntu/add_server_repositories.sh
@@ -6,7 +6,7 @@ set -eux
 sudo apt-get install -y software-properties-common
 
 echo 'Neovim repo'
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:neovim-ppa/stable
 
 echo 'Fish repo'
 sudo apt-add-repository -y ppa:fish-shell/release-3
