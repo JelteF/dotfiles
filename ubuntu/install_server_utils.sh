5b9e843ead9be99c2810ff5cdeaeeb917eb0ec1a
Jelte Fennema <github-tech@jeltef.nl>
2023-03-15 11:21:32 +0100

    Update manual downloads

--- ubuntu/install_server_utils.sh
+++ ubuntu/install_server_utils.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+set -euxo pipefail
 
 # Install everything
 echo 'Installing important stuff'
@@ -16,13 +16,13 @@ curl_deb() {
 }
 
 curl_deb https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-curl_deb https://github.com/sharkdp/fd/releases/download/v8.4.0/fd_8.4.0_amd64.deb
-curl_deb https://github.com/dandavison/delta/releases/download/0.13.0/git-delta-musl_0.13.0_amd64.deb
-curl_deb https://github.com/sharkdp/bat/releases/download/v0.21.0/bat_0.21.0_amd64.deb
-curl_deb https://github.com/cli/cli/releases/download/v2.14.3/gh_2.14.3_linux_amd64.deb
+curl_deb https://github.com/sharkdp/fd/releases/download/v8.7.0/fd_8.7.0_amd64.deb
+curl_deb https://github.com/dandavison/delta/releases/download/0.15.1/git-delta-musl_0.15.1_amd64.deb
+curl_deb https://github.com/sharkdp/bat/releases/download/v0.22.1/bat_0.22.1_amd64.deb
+curl_deb https://github.com/cli/cli/releases/download/v2.24.3/gh_2.24.3_linux_amd64.deb
 
 mkdir -p ~/.bin
-curl --location https://github.com/starship/starship/releases/download/v1.8.0/starship-x86_64-unknown-linux-musl.tar.gz | tar xz --directory ~/.bin starship
+curl --location https://github.com/starship/starship/releases/download/v1.13.1/starship-x86_64-unknown-linux-musl.tar.gz | tar xz --directory ~/.bin starship
 
 wget https://github.com/jstarks/npiperelay/releases/latest/download/npiperelay_windows_amd64.zip
 unzip -o npiperelay_windows_amd64.zip -d ~/npiperelay
