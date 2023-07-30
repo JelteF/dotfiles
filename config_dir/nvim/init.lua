1ef0ff44f408b732eec687f491f3b91bfb6d82b3
Jelte Fennema <github-tech@jeltef.nl>
2023-07-30 16:34:57 +0200

    Change from ccls to clangd

--- config_dir/nvim/init.lua
+++ config_dir/nvim/init.lua
@@ -463,8 +463,8 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  ccls = {},
+  clangd = {},
+  -- ccls = {}, -- not supported by mason
   gopls = {},
   pyright = {},
   rust_analyzer = {},
