84c30769525f2be28c5a50b81184dc22363f3075
Jelte Fennema <github-tech@jeltef.nl>
2023-07-31 11:47:25 +0200

    Make treesj work

--- config_dir/nvim/init.lua
+++ config_dir/nvim/init.lua
@@ -515,6 +515,8 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
+local tsj = require('treesj').setup {}
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
