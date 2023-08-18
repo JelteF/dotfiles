93a9ccde18bf482225b877b38f256d943f9e6954
Jelte Fennema <github-tech@jeltef.nl>
2023-08-18 16:44:06 +0200

    Some small changes

--- ubuntu/add_server_repositories.sh
+++ ubuntu/add_server_repositories.sh
@@ -6,7 +6,10 @@ set -eux
 sudo apt-get install -y software-properties-common
 
 echo 'Neovim repo'
-sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 
 echo 'Fish repo'
 sudo apt-add-repository -y ppa:fish-shell/release-3
+
+echo 'Add nodejs'
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
