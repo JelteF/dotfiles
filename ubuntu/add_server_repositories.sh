9acfaa9d69ada20c820d3c5553e56597d5eaad5b
Jelte Fennema-Nio <github-tech@jeltef.nl>
2024-07-14 23:46:47 +0200

    Add docker repo

--- ubuntu/add_server_repositories.sh
+++ ubuntu/add_server_repositories.sh
@@ -13,3 +13,14 @@ sudo apt-add-repository -y ppa:fish-shell/release-3
 
 echo 'Add nodejs'
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
